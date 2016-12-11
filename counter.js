452ce1eb3058663a09375d96ad48e5104156e07d
sanket001 <sanket.shrotri@ssla.edu.in>
2016-12-11 10:19:00 +0530

    Added a third column for select button
    As of now it only shows the text select. Will need to convert it into a
    button

--- counter.js
+++ counter.js
@@ -2,10 +2,18 @@ new Vue({
     el: '#app',
     data: {
         searchQuery: '',
-        gridColumns: ['dish', 'calories'],
+        gridColumns: ['dish', 'calories', 'button'],
         foods: [
-          { dish: 'Pizza', calories: Infinity },
-          { dish: 'Chinese', calories: 500 }
+          { dish: 'Pizza', calories: Infinity, button:'select' },
+          { dish: 'Chinese', calories: 500, button:'select' },
+          { dish: 'Idli', calories: 500, button:'select' },
+          { dish: 'Dosa', calories: 500, button:'select' },
+          { dish: 'Coconut', calories: 500, button:'select' },
+          { dish: 'Milk', calories: 500, button:'select' },
+          { dish: 'Bread', calories: 500, button:'select' },
+          { dish: 'Noodles', calories: 500, button:'select' },
+          { dish: 'Tofu', calories: 500, button:'select' },
+          { dish: 'Dal Fry', calories: 500, button:'select' }
         ]
     }
 })
